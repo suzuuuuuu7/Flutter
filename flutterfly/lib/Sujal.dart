b298d1fb99def25317039e71171aa66bef90fa01
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-19 21:15:53 +0545

    Updated in Sujal.dart

--- flutterfly/lib/Sujal.dart
+++ flutterfly/lib/Sujal.dart
@@ -1,3 +1,14 @@
+import "dart:io";
+
 void main() {
-  print("Hello sujal");
+  print("This is your identification");
+  print("Enter your age");
+  int age = int.parse(stdin.readLineSync()!);
+  print("Enter your name");
+  String name = stdin.readLineSync()!;
+  print("Enter your address");
+  var address = stdin.readLineSync();
+  print("Your name is:$name");
+  print("Age:$age");
+  print("Address:$address");
 }
