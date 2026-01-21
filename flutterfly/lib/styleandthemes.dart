b5922696cba3b54e0f387819cfb9f19198dda361
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-21 18:40:13 +0545

    uPDATED

--- flutterfly/lib/styleandthemes.dart
+++ flutterfly/lib/styleandthemes.dart
@@ -14,28 +14,30 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.red,
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
         textTheme: TextTheme(
-          headlineLarge: TextStyle(fontSize: 40, fontStyle: FontStyle.italic),
-          titleSmall: TextStyle(fontSize: 20, color: Colors.green),
+          headlineSmall: TextStyle(color: Colors.red, fontSize: 50),
+          titleSmall: TextStyle(color: Colors.green, fontSize: 100),
         ),
-      ),
 
-      // This is the theme of your application.
-      //
-      // TRY THIS: Try running your application with "flutter run". You'll see
-      // the application has a purple toolbar. Then, without quitting the app,
-      // try changing the seedColor in the colorScheme below to Colors.green
-      // and then invoke "hot reload" (save your changes or press the "hot
-      // reload" button in a Flutter-supported IDE, or press "r" if you used
-      // the command line to start the app).
-      //
-      // Notice that the counter didn't reset back to zero; the application
-      // state is not lost during the reload. To reset the state, use hot
-      // restart instead.
-      //
-      // This works for code too, not just values: Most code changes can be
-      // tested with just a hot reload.
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -72,28 +74,27 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-
+        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
         children: [
-          Text("Hello"),
-          Text("Hello", style: Theme.of(context).textTheme.headlineLarge),
-
-          Text("Sual kc", style: Theme.of(context).textTheme.titleSmall),
           Text(
-            "You can do this",
-            style: Theme.of(context).textTheme.headlineLarge,
+            "Hello Sujal..",
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          Text(
+            "Thank you so much",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          Text(
+            "Its me karuna.",
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
 
-          Text(
-            "Not immediately",
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-          Text("But", style: Theme.of(context).textTheme.titleSmall),
-          Text("Definitely", style: Theme.of(context).textTheme.headlineLarge),
+          Text("Bye Bye", style: Theme.of(context).textTheme.titleSmall),
         ],
       ),
     );
