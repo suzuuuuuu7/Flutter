ae8d2097e92b534633d7774ba25943057a2502e4
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-28 20:15:33 +0545

    Call back function

---
+++ flutterfly/lib/callback.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Welcome to Flutter'),
+
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  callback() {
+    print("button clicked!!;"); // function declaration
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 120,
+
+        leading: Padding(
+          padding: EdgeInsets.only(bottom: 25),
+          child: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              print("Menu clicked");
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 25.0),
+            child: IconButton(
+              onPressed: () {
+                print("Setting opened");
+              },
+              icon: Icon(Icons.settings),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25.0),
+            child: IconButton(
+              onPressed: () {
+                print("chekc notification");
+              },
+              icon: Icon(Icons.notification_add),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25),
+            child: IconButton(
+              onPressed: () {
+                print("Find more option.");
+              },
+              icon: Icon(Icons.more_vert),
+            ),
+          ),
+        ],
+        backgroundColor: Colors.redAccent,
+        title: Padding(
+          padding: EdgeInsets.only(bottom: 25, right: 50),
+          child: Text(
+            "FlutteRflY",
+            style: TextStyle(color: Colors.black, fontSize: 27),
+          ),
+        ),
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          callback(); // call back function
+        },
+        child: Text("Click me"),
+      ),
+    ); // This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
